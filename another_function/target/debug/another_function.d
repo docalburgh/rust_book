b3c1905d51cdfc0e1020e37ab777aae4b3d845e5
Doc <docalburgh@proton.me>
2024-10-25 16:22:15 -0500

    this commit adds another_function to main.

---
+++ another_function/target/debug/another_function.d
@@ -0,0 +1,1 @@
+/home/andy/projects/another_function/target/debug/another_function: /home/andy/projects/another_function/src/main.rs
---
+++ another_function/target/debug/deps/another_function-c871e85ae247bb0a.d
@@ -0,0 +1,5 @@
+/home/andy/projects/another_function/target/debug/deps/another_function-c871e85ae247bb0a: src/main.rs
+
+/home/andy/projects/another_function/target/debug/deps/another_function-c871e85ae247bb0a.d: src/main.rs
+
+src/main.rs:
